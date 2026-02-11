486816dd41a22e38becd3fc93a601c734795fee9
Himadrimehra <himadrimehra2005@gmail.com>
2026-02-11 11:56:27 +0000

    Code challenge submission for question: 40 Combination Sum II

---
+++ 40 Combination Sum II/40combination-sum-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void solve(int index, int target, vector<int>& candidates,
+               vector<int>& ds, vector<vector<int>>& ans) {
+                if(target<0) return;
+
+        if (target == 0) {
+            ans.push_back(ds);
+            return;
+        }
+
+        for (int i = index; i < candidates.size(); i++) {
+
+           
+            if (i > index && candidates[i] == candidates[i - 1])
+                continue;
+
+            ds.push_back(candidates[i]);
+            solve(i + 1, target - candidates[i], candidates, ds, ans);
+            ds.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end()); 
+        vector<vector<int>> ans;
+        vector<int> ds;
+        solve(0, target, candidates, ds, ans);
+        return ans;
+    }
+};
