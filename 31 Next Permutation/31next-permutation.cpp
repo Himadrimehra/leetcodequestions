731185aca082f7acba802097d34c629edc744afa
Himadrimehra <himadrimehra2005@gmail.com>
2026-06-14 01:43:33 +0000

    Code challenge submission for question: 31 Next Permutation

---
+++ 31 Next Permutation/31next-permutation.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int index = -1;
+        int n = nums.size();
+
+        // Find breakpoint
+        for(int i = n - 2; i >= 0; i--) {
+            if(nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+
+        // If no breakpoint exists
+        if(index == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        // Find next greater element
+        for(int i = n - 1; i > index; i--) {
+            if(nums[i] > nums[index]) {
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+
+        // Reverse remaining part
+        reverse(nums.begin() + index + 1, nums.end());
+    }
+};
