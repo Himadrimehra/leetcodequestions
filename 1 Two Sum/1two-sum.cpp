b657aaf5d4eec2cae6bf55582b0f71ce97239fd9
Himadrimehra <himadrimehra2005@gmail.com>
2026-06-24 01:01:28 +0000

    Code challenge submission for question: 1 Two Sum

---
+++ 1 Two Sum/1two-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++)
+        {
+            int moreneed=target-nums[i];
+            if(mp.find(moreneed)!=mp.end()){
+                return{mp[moreneed],i};
+            }
+            else 
+            {
+                mp[nums[i]]=i;
+            }
+        }
+        return {};
+    }
+};
