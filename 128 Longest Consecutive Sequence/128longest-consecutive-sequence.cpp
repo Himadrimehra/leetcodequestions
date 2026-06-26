8be69ed9fbcc3c965bafea609f8c24bdc3d99aed
Himadrimehra <himadrimehra2005@gmail.com>
2026-06-26 00:45:03 +0000

    Code challenge submission for question: 128 Longest Consecutive Sequence

---
+++ 128 Longest Consecutive Sequence/128longest-consecutive-sequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+       unordered_set<int>st;
+       int n=nums.size();
+       if(n==0)return 0;
+        int longest=1;
+        for(auto num:nums)
+        {
+            st.insert(num);
+        }
+        for(auto it:st)
+        {
+            if(st.find(it-1)==st.end())
+            {
+               int count =1;
+                int x=it;
+            
+            while(st.find(x+1)!=st.end())
+            {
+                x=x+1;
+                count++;
+            }
+            longest=max(longest,count);
+        }
+        }
+        return longest;
+    }
+};
