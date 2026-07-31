b5ee10a894d80536281f0c021c0ddc382bb215ad
Himadri Mehra <himadrimehra2005@gmail.com>
2026-07-31 11:48:00 +0530

    Time: 0 ms (100%), Space: 9.5 MB (11.02%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,27 +1,63 @@
 class Solution {
 public:
+    vector<int>buildlps(string needle)
+    {
+        int n=needle.size();
+        vector<int>lps(n,0);
+        int i=0;
+        int j=1;
+        while(j<n)
+        {
+           if(needle[i]==needle[j])
+           {
+             i++;
+             lps[j]=i;
+             j++;
+           }
+           else 
+           {
+            if(i==0)
+            {
+                lps[j]=0;
+                j++;
+            }
+            else 
+            {
+                i=lps[i-1];
+            }
+           }
+        }
+        return lps ;
+    }
     int strStr(string haystack, string needle) {
-
-        int n = haystack.size();
-        int m = needle.size();
-
-        for (int i = 0; i <= n - m; i++) {
-
-            if (haystack[i] == needle[0]) {
-
-                int l = i;
-                int k = 0;
-
-                while (k < m && l < n && haystack[l] == needle[k]) {
-                    l++;
-                    k++;
-                }
-
-                if (k == m)
-                    return i;
+        int n=haystack.size();
+        int m=needle.size();
+        vector<int>lps=buildlps(needle);
+       int i=0;
+       int j=0;
+       while(i<n)
+       {
+        if(haystack[i]==needle[j])
+        {
+            i++;
+            j++;
+            if(j==m)
+            {
+                return i-j;
             }
         }
-
-        return -1;
+        else 
+        {
+            if(j==0)
+            {
+               i++;
+            }
+            else 
+            {
+                j=lps[j-1];
+            }
+        }
+       }
+       return -1;
     }
 };
