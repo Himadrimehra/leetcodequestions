80c133604918243529295f9fd81b3c534780d286
Himadri Mehra <himadrimehra2005@gmail.com>
2026-09-17 17:53:09 +0530

    Time: 0 ms (100%), Space: 10.2 MB (70.12%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -6,13 +6,12 @@ public:
         {
             for(int j=i+1;j<n;j++)
             {
-                swap(matrix[i][j],matrix[j][i]);
+               swap(matrix[i][j],matrix[j][i]);
             }
         }
-        //reverse
-       for(int i=0;i<n;i++)
-       {
-        reverse(matrix[i].begin(),matrix[i].end());
-       }
+        for(int i=0;i<n;i++)
+        {
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
     }
 };
